bb1332d4f63d549dc76ced1d4dc0588c022b42a4
VitorHasc <136938842+VitorHasc@users.noreply.github.com>
2024-08-27 22:25:23 -0300

    Add files via upload

---
+++ src/controllers/userController.js
@@ -0,0 +1,40 @@
+const SECRET = "1234"
+const jwt = require('jsonwebtoken');
+const { registrarUser, deletarUser } = require('../models/userModel');
+
+const entrar = async (req, res) => {
+    const {nome} = req.body;
+    const a = await registrarUser(req.body);
+    const token = jwt.sign({ nome: nome }, SECRET, { expiresIn: '48h' });
+    res.json(token);
+  } 
+    
+const sair = async (req, res) => {
+    try {
+    
+        const token = req.headers.authorization;
+        console.log(token);
+        if (!token) {
+            return res.status(401).send('No token provided.');
+        }
+
+        // Verify the token 
+        const decoded = jwt.verify(token, SECRET);
+        const { nome } = decoded;
+        console.log(nome);
+        await deletarUser(nome);
+
+        res.json({
+          status: 200,
+          msg: "OK"
+        });
+    } catch (error) {
+        console.error('Error during logout:', error);
+        res.status(401).send('Invalid token.');
+    }
+};
+    
+  module.exports = {
+    entrar,
+    sair
+  };
